a6a4c5b51fe93bcf8e795e5592b02dc436324412
wuzongyu <ktsvook@gmail.com>
2021-09-27 21:01:38 +0800

    '文件编程 认识FileChannel，Path，Files三个类'

---
+++ src/test/java/netty/TestFileChannerTransferTo.java
@@ -0,0 +1,24 @@
+package netty;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+//文件编程的TransfereTo()方法
+public class TestFileChannerTransferTo {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("file\\date.txt");
+             FileOutputStream fos = new FileOutputStream("file\\student.txt");
+             FileChannel inputChannel = fis.getChannel();
+             FileChannel outputChannel = fos.getChannel()) {
+            // 参数：inputChannel的起始位置，传输数据的大小，目的channel
+            // 返回值为传输的数据的字节数
+            // transferTo一次只能传输2G的数据
+            inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/test/java/netty/TestFilesWalkFileTree.java
@@ -0,0 +1,56 @@
+package netty;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TestFilesWalkFileTree {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("D:\\Programs\\Netty-demo");
+        // 文件目录数目
+        AtomicInteger dirCount = new AtomicInteger();
+        // 文件数目
+        AtomicInteger fileCount = new AtomicInteger();
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("===>" + dir);
+                // 增加文件目录数
+                dirCount.incrementAndGet();
+                return super.preVisitDirectory(dir, attrs);
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.println(file);
+                // 增加文件数
+                fileCount.incrementAndGet();
+                return super.visitFile(file, attrs);
+            }
+        });
+        // 打印数目
+        System.out.println("文件目录数:" + dirCount.get());
+        System.out.println("文件数:" + fileCount.get());
+
+    }
+
+/*    public static void main(String[] args) throws IOException {
+        //Files.delete(Paths.get("D:\\Snipaste-1.16.2-x64 –副本"));
+        Files.walkFileTree(Paths.get("D: \\Snipaste-1.16.2-x64 -副本"), new SimpleFileVisitor<Path>() {
+            @Override
+
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }*/
+}
